1b7e4fcb7f39c3a8f8c8046d70babef626d06b13
legin8 <maynne1@student.op.ac.nz>
2023-04-28 14:41:50 +1200

    assessment 1: added Randomization

--- Assets/Scripts/Minimax.cs
+++ Assets/Scripts/Minimax.cs
@@ -162,6 +162,7 @@ public class Minimax : MonoBehaviour
         {
             int maxScore = int.MinValue;
             List<MoveData> allMoves = GetMoves(gameManager.playerTurn);
+            allMoves = Shuffle(allMoves);
             foreach (MoveData move in allMoves)
             {
                 moveStack.Push(move);
@@ -186,6 +187,7 @@ public class Minimax : MonoBehaviour
             PlayerTeam opponent = gameManager.playerTurn == PlayerTeam.WHITE ? PlayerTeam.BLACK : PlayerTeam.WHITE;
             int minScore = int.MaxValue;
             List<MoveData> allMoves = GetMoves(opponent);
+            allMoves = Shuffle(allMoves);
             foreach (MoveData move in allMoves)
             {
                 moveStack.Push(move);
@@ -201,6 +203,17 @@ public class Minimax : MonoBehaviour
         }
     }
 
-
+    public List<T> Shuffle<T>(List<T> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = Random.Range(0,n);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+        return list;
+    }
 
 }
